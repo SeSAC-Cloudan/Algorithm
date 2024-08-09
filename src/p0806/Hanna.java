136bcc1dbbbcda82ccac2692937d9604510cbc2b
12ka39 <work_lee2@naver.com>
2024-08-09 18:24:49 +0900

    [한나] 모스부호

--- src/p0806/Hanna.java
+++ src/p0806/Hanna.java
@@ -1,4 +1,35 @@
 package p0806;
 
+import java.util.Scanner;
+
 public class Hanna {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("모스부호를 입력하세요");
+        String letter = scan.nextLine();
+        morse(letter);
+    }
+
+    public static void morse(String letter) {
+        String [] morseCodes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",
+                ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        StringBuilder sb = new StringBuilder();
+
+        String[] letters = letter.split("\\s+");
+
+        char ans = 0;
+
+        for (String ch : letters) {
+            for(int i=0; i<morseCodes.length; i++) {
+                if(ch.equals(morseCodes[i])) {
+                    ans = (char)(97 + i); // a의 아스키 코드 97
+                    sb = sb.append(ans);
+                    break; // 모스 부호 매칭됨 (루프 돌 필요가 없음)
+
+                }
+            }
+        }
+        System.out.println(sb);
+    }
 }
