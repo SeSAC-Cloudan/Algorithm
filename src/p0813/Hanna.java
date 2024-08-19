f7e562a890ad2cc64a8ddd61007913582b68ff26
123 <121006533+12ka39@users.noreply.github.com>
2024-08-19 18:20:16 +0900

    [한나] 등수 매기기 (#10)
    * [한나] 등수 매기기
    
    * [한나] 등수 매기기(수정)
    
    * [한나] 등수 매기기

--- src/p0813/Hanna.java
+++ src/p0813/Hanna.java
@@ -1,4 +1,22 @@
 package p0813;
 
+import java.util.*;
+
 public class Hanna {
+    public int[] solution(int[][] score) {
+        int l = score.length;
+        List<Integer> list = new ArrayList<>(); // 점수 담을 arraylist
+        for (int[] x : score)
+            list.add(x[0] + x[1]); //영어+수학 점수
+
+        list.sort(Collections.reverseOrder()); // 내림차순 정렬
+
+        int[] result = new int[l];
+
+        for (int i = 0; i < l; i++)
+            result[i] = list.indexOf(score[i][0] + score[i][1]) + 1;
+                        //합에 해당하는 인덱스 + 1
+
+        return result;
+    }
 }
