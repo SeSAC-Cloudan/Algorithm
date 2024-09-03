0e8f9fb0938f033cb84216627b3a13fc785e1e54
123 <121006533+12ka39@users.noreply.github.com>
2024-09-03 17:50:00 +0900

    [한나] 가장 가까운 같은 글자 (#34)
    * [한나] 가장 가까운 같은 글자

--- src/p0903/Hanna.java
+++ src/p0903/Hanna.java
@@ -1,4 +1,38 @@
 package p0903;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Hanna {
+    public static void main(String[] args) {
+        String s = "banana";
+        int[] result = solution(s);
+
+        // 결과 출력
+        for (int r : result) {
+            System.out.print(r + " ");
+        }
+    }
+
+    public static int[] solution(String s) {
+        int[] answer = new int[s.length()]; // 정답 담을 배열
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char piece = s.charAt(i); // i번째를 char 타입으로 변경
+
+            // map에 현재 문자가 존재한다면
+            if (map.containsKey(piece)) {
+                // 현재 위치와 이전 위치의 차이를 저장
+                answer[i] = i - map.get(piece);
+            } else {
+                // 처음 등장하는 문자일 경우 -1 저장
+                answer[i] = -1;
+            }
+            // 현재 문자의 위치를 map에 저장 (업데이트)
+            map.put(piece, i);
+        }
+        return answer;
+    }
+
 }
