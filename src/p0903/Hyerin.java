a2e35de46b72af3054415a9b9bf016572631aad8
unknown <lhl9603@gmail.com>
2024-09-03 13:59:34 +0900

    [혜린] 가장 가까운 같은 글자

--- src/p0903/Hyerin.java
+++ src/p0903/Hyerin.java
@@ -1,4 +1,24 @@
 package p0903;
 
 public class Hyerin {
+    public int[] solution(String s) {
+        int length = s.length();
+        int[] answer = new int[length];
+
+        for(int i = 0; i < s.length(); i++) {
+            answer[i] = -1;
+        }
+
+        for(int i=0; i<length; i++){
+            char currentChar = s.charAt(i);
+            for (int j = i - 1; j >= 0; j--) {
+                if (s.charAt(j) == currentChar) {
+                    answer[i] = i - j;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
 }
